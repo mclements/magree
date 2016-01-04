de06db965399f85192a37ceadc38516836b6614d
Mark Clements <mark.clements@ki.se>
2016-01-04 16:21:55 +0100

    Minor updates

--- R/oconnell.R
+++ R/oconnell.R
@@ -1,15 +1,26 @@
-oconnell <-
-function(X,score=NULL, i=3) {
+oconnell <- function(X,score=NULL, weights=c("unweighted","linear","quadratic"), i=NULL) {
+    ## check arguments
+    weights <- match.arg(weights)
+    stopifnot(inherits(X,"data.frame") || inherits(X,"matrix"))
+    stopifnot(ncol(X)>1 && nrow(X)>1)
+    ## if i is specified, then it takes precedence over weights, else:
+    if (is.null(i)) i <- switch(weights,unweighted=1,linear=2,quadratic=3)
+    matX <- as.matrix(X)
+    ## check for NAs, 
+    if (any(is.na(matX)))
+        stop("NAs in matrix: missing values or columns of different types?")
+    score.labels <- sort(unique(as.vector(matX)))
+    if (is.null(score)) score <- as.numeric(score.labels)
+    if (any(is.na(score)))
+        stop("Missing or non-numeric scores. Possibly specify the score argument")
     nrater <- ncol(X); nsubj <- nrow(X)
     raterNames <- colnames(X)
     itemNames <- rownames(X)
-    X <- as.matrix(X)
-    score.labels <- sort(unique(as.vector(X)))
-    if (is.null(score)) score <- score.labels
-    X <- matrix(as.integer(as.factor(X)),nrow(X),ncol(X))
+    ## NOTE: resp is X -> factor -> integer -> matrix -> transpose
+    resp <- t(matrix(as.integer(as.factor(matX)),nrow(X),ncol(X)))
     nscore <- length(score)
     out <- .Fortran("oconnell", nrater=nrater, nsubj=nsubj, nscore=nscore, i=as.integer(i),
-                    resp=t(X), s1=rep(0,nsubj), s2=rep(0,nsubj),
+                    resp=resp, s1=rep(0,nsubj), s2=rep(0,nsubj),
                     p1=matrix(0,nrater,nscore),p2=rep(0,nscore),w1=matrix(0,nrater,nscore),
                     w2=matrix(0,nrater,nscore),
                     score=as.double(score),delta=matrix(0,nrater,nrater),
@@ -18,23 +29,49 @@ function(X,score=NULL, i=3) {
     colnames(out$resp) <- names(out$s1) <- names(out$s2) <- names(out$d) <- itemNames
     rownames(out$resp) <- rownames(out$p1) <- rownames(out$w1) <- rownames(out$w2) <- rownames(out$delta) <- colnames(out$delta) <- raterNames
     colnames(out$p1) <- names(out$p2) <- colnames(out$w1) <- names(score) <- score.labels
+    out$p0sav1 <- 2*pnorm(abs(out$sav1/sqrt(out$v0sav1)-1),lower.tail=FALSE)
+    out$p0sav2 <- 2*pnorm(abs(out$sav2/sqrt(out$v0sav2)-1),lower.tail=FALSE)
     out$X <- X
-    out$resp <- NULL
+    out$call <- sys.call()
     class(out) <- "oconnell"
     out
 }
 
-print.oconnell <- function(obj, ...) {
-    cat(sprintf("O'Connell-Dobson estimator\n\nSav(hetero):\t%f (se=%f)\nSav(homoge):\t%f (se=%f)\n",
-            obj$sav1,
-            sqrt(obj$vsav1),
-            obj$sav2,
-            sqrt(obj$vsav2),...))
-    cat(sprintf("Pr(Overall agreement due to chance | hetero):\t%g\n",pnorm(abs(obj$sav1/sqrt(obj$v0sav1)-1),lower.tail=FALSE)), ...)
-    cat(sprintf("Pr(Overall agreement due to chance | homoge):\t%g\n",pnorm(abs(obj$sav2/sqrt(obj$v0sav2)-1),lower.tail=FALSE)), ...)
+summary.oconnell <- function(obj, ci.transform=c("logit","identity"), ci.p=0.95, ...) {
+    ci.transform <- match.arg(ci.transform)
+    expit <- function(x) 1/(1+exp(-x))
+    logit <- function(x) log(x/(1-x))
+    alpha <- 1-ci.p
+    transf <- switch(ci.transform,
+                     identity=function(mean,se) mean+qnorm(c(alpha/2,1-alpha/2))*se,
+                     logit=function(mean,se)
+                     expit(logit(mean)+qnorm(c(alpha/2,1-alpha/2))*se/mean/(1-mean)))
+    obj$ci1 <- transf(obj$sav1,sqrt(obj$vsav1))
+    obj$ci2 <- transf(obj$sav2,sqrt(obj$vsav2))
+    structure(obj, class="summary.oconnell")
 }
 
-summary.oconnell <- function(obj) structure(obj, class="summary.oconnell") # it's all there already
+print.oconnell <- function(obj, ...) {
+    weight.labels <- c("unweighted","linear weights","quadratic weights")
+    if (!inherits(obj,"summary.oconnell")) obj <- summary(obj,...)
+    cat(sprintf("O'Connell-Dobson estimator (%s)\n\nSav(hetero):\t%f (se: %f; 95%% CI: %f, %f)\nSav(homoge):\t%f (se: %f; 95%% CI: %f, %f)\n",
+                weight.labels[obj$i],
+                obj$sav1,
+                sqrt(obj$vsav1),
+                obj$ci1[1],
+                obj$ci1[2],
+                obj$sav2,
+                sqrt(obj$vsav2),
+                obj$ci2[1],
+                obj$ci2[2],
+                ...))
+    cat(sprintf("Pr(Overall agreement due to chance | hetero):\t%s\n",
+                format.pval(obj$p0sav1), ...))
+    cat(sprintf("Pr(Overall agreement due to chance | homoge):\t%s\n",
+                format.pval(obj$p0sav2), ...))
+    invisible(obj)
+}
+
 print.summary.oconnell <- function(obj, ...) {
     print.oconnell(obj,...)
     cat("\nObserved marginal distributions for categories:\n\n")
@@ -43,75 +80,15 @@ print.summary.oconnell <- function(obj, ...) {
     print(obj$p1)
     cat("\nAgreement statistics S_i for the individual items:\n\n")
     print(obj$s1)
+    invisible(obj)
 }
 
-
-## Landis and Kock (1977) - output this to ./data/?
-landis <- structure(c(4L, 1L, 3L, 4L, 3L, 2L, 1L, 3L, 2L, 1L, 5L, 1L, 3L, 
-2L, 4L, 3L, 2L, 2L, 2L, 1L, 4L, 1L, 1L, 2L, 4L, 3L, 3L, 1L, 4L, 
-3L, 1L, 3L, 2L, 3L, 5L, 2L, 3L, 3L, 5L, 5L, 3L, 1L, 2L, 4L, 3L, 
-3L, 2L, 3L, 4L, 3L, 3L, 2L, 2L, 1L, 3L, 1L, 1L, 4L, 1L, 2L, 4L, 
-3L, 1L, 2L, 3L, 1L, 4L, 3L, 3L, 4L, 1L, 2L, 2L, 2L, 4L, 1L, 4L, 
-5L, 2L, 4L, 3L, 4L, 4L, 2L, 3L, 3L, 4L, 3L, 1L, 3L, 4L, 4L, 1L, 
-3L, 4L, 3L, 1L, 2L, 3L, 2L, 3L, 3L, 2L, 1L, 3L, 3L, 2L, 3L, 1L, 
-3L, 3L, 1L, 1L, 2L, 5L, 4L, 1L, 2L, 3L, 1L, 3L, 3L, 3L, 1L, 1L, 
-3L, 2L, 1L, 5L, 1L, 3L, 2L, 3L, 3L, 3L, 1L, 3L, 1L, 3L, 1L, 1L, 
-1L, 4L, 3L, 3L, 1L, 3L, 3L, 1L, 3L, 2L, 3L, 3L, 1L, 3L, 3L, 5L, 
-3L, 2L, 1L, 3L, 4L, 3L, 2L, 3L, 3L, 3L, 3L, 3L, 2L, 3L, 1L, 3L, 
-1L, 3L, 3L, 3L, 3L, 3L, 3L, 1L, 3L, 3L, 1L, 3L, 3L, 3L, 3L, 2L, 
-2L, 3L, 1L, 4L, 1L, 4L, 5L, 3L, 4L, 3L, 3L, 2L, 3L, 3L, 3L, 4L, 
-3L, 1L, 3L, 3L, 3L, 2L, 3L, 4L, 3L, 1L, 3L, 3L, 3L, 3L, 3L, 2L, 
-1L, 3L, 3L, 3L, 3L, 1L, 3L, 3L, 1L, 1L, 2L, 3L, 3L, 1L, 3L, 4L, 
-1L, 3L, 3L, 3L, 2L, 1L, 2L, 2L, 1L, 5L, 1L, 3L, 2L, 3L, 2L, 2L, 
-2L, 2L, 2L, 3L, 2L, 1L, 2L, 4L, 3L, 3L, 1L, 3L, 3L, 1L, 3L, 2L, 
-2L, 3L, 1L, 2L, 3L, 5L, 3L, 2L, 1L, 1L, 4L, 3L, 2L, 2L, 3L, 3L, 
-2L, 3L, 2L, 2L, 1L, 3L, 2L, 2L, 3L, 2L, 2L, 3L, 3L, 1L, 2L, 2L, 
-1L, 3L, 3L, 3L, 1L, 1L, 1L, 2L, 1L, 3L, 1L, 3L, 1L, 2L, 4L, 2L, 
-3L, 3L, 2L, 3L, 2L, 3L, 2L, 2L, 3L, 1L, 3L, 2L, 3L, 3L, 2L, 1L, 
-2L, 3L, 1L, 2L, 2L, 1L, 1L, 2L, 2L, 1L, 2L, 1L, 3L, 2L, 1L, 1L, 
-1L, 4L, 4L, 1L, 1L, 2L, 1L, 3L, 4L, 3L, 1L, 1L, 3L, 2L, 1L, 4L, 
-1L, 2L, 1L, 2L, 3L, 2L, 1L, 2L, 1L, 4L, 1L, 1L, 2L, 2L, 2L, 3L, 
-1L, 3L, 3L, 1L, 2L, 2L, 2L, 3L, 1L, 2L, 3L, 5L, 2L, 2L, 1L, 2L, 
-3L, 2L, 2L, 2L, 4L, 3L, 2L, 3L, 1L, 2L, 1L, 3L, 1L, 1L, 3L, 2L, 
-2L, 3L, 4L, 1L, 2L, 3L, 1L, 3L, 2L, 3L, 3L, 1L, 2L, 1L, 2L, 2L, 
-1L, 3L, 4L, 2L, 2L, 3L, 3L, 2L, 2L, 2L, 1L, 2L, 2L, 1L, 2L, 1L, 
-4L, 1L, 2L, 4L, 2L, 1L, 2L, 3L, 1L, 2L, 2L, 1L, 1L, 2L, 2L, 1L, 
-2L, 1L, 2L, 2L, 1L, 1L, 1L, 2L, 2L, 1L, 1L, 3L, 1L, 3L, 3L, 3L, 
-1L, 2L, 2L, 3L, 2L, 5L, 2L, 3L, 1L, 3L, 3L, 3L, 2L, 2L, 1L, 3L, 
-2L, 1L, 2L, 4L, 3L, 3L, 1L, 3L, 3L, 1L, 3L, 3L, 3L, 4L, 2L, 3L, 
-3L, 5L, 3L, 2L, 2L, 3L, 3L, 3L, 2L, 2L, 3L, 3L, 4L, 3L, 2L, 3L, 
-1L, 3L, 1L, 2L, 3L, 2L, 3L, 3L, 3L, 1L, 3L, 3L, 1L, 3L, 3L, 3L, 
-3L, 1L, 2L, 3L, 1L, 4L, 1L, 4L, 5L, 2L, 5L, 3L, 3L, 3L, 4L, 4L, 
-3L, 4L, 3L, 2L, 4L, 2L, 4L, 2L, 4L, 4L, 3L, 1L, 4L, 3L, 3L, 3L, 
-3L, 2L, 2L, 2L, 2L, 2L, 3L, 2L, 3L, 3L, 2L, 1L, 2L, 3L, 4L, 2L, 
-2L, 3L, 1L, 3L, 3L, 3L, 1L, 1L, 2L, 1L, 1L, 5L, 1L, 3L, 1L, 2L, 
-3L, 2L, 1L, 1L, 1L, 3L, 1L, 1L, 1L, 3L, 2L, 2L, 1L, 2L, 3L, 1L, 
-1L, 1L, 1L, 1L, 1L, 1L, 2L, 5L, 2L, 1L, 1L, 1L, 3L, 2L, 1L, 2L, 
-2L, 5L, 2L, 2L, 2L, 1L, 1L, 3L, 1L, 1L, 2L, 1L, 2L, 3L, 2L, 1L, 
-2L, 1L, 1L, 3L, 1L, 2L, 2L, 1L, 1L, 2L, 1L, 1L, 1L, 3L, 5L, 1L, 
-1L, 3L, 3L, 2L, 1L, 2L, 2L, 1L, 2L, 1L, 3L, 1L, 3L, 1L, 2L, 3L, 
-3L, 1L, 1L, 2L, 1L, 2L, 1L, 1L, 1L, 2L, 1L, 1L, 2L, 1L, 2L, 1L, 
-1L, 1L, 1L, 4L, 1L, 1L, 1L, 3L, 1L, 3L, 3L, 3L, 1L, 1L, 3L, 2L, 
-1L, 5L, 1L, 3L, 2L, 3L, 3L, 3L, 1L, 3L, 1L, 3L, 1L, 1L, 2L, 3L, 
-3L, 3L, 1L, 3L, 3L, 1L, 3L, 2L, 3L, 3L, 1L, 3L, 3L, 5L, 3L, 2L, 
-1L, 3L, 3L, 3L, 1L, 2L, 3L, 3L, 3L, 3L, 2L, 3L, 1L, 3L, 1L, 1L, 
-3L, 2L, 3L, 3L, 4L, 1L, 2L, 3L, 1L, 3L, 3L, 3L, 3L, 1L, 2L, 2L, 
-1L, 3L, 1L, 3L, 4L, 2L, 3L, 3L, 3L, 3L, 3L, 3L, 2L, 3L, 2L, 1L, 
-3L, 2L, 3L, 2L, 3L, 4L, 3L, 1L, 2L, 3L, 1L, 3L, 3L, 1L, 1L, 3L, 
-2L, 1L, 3L, 1L, 3L, 3L, 1L, 1L, 2L, 3L, 3L, 1L, 2L), .Dim = c(118L, 
-7L), .Dimnames = list(c("1", "2", "3", "4", "5", "6", "7", "8", 
-"9", "10", "11", "12", "13", "15", "16", "17", "18", "19", "22", 
-"23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", 
-"34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", 
-"45", "46", "47", "48", "49", "51", "52", "53", "54", "55", "56", 
-"57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", 
-"68", "69", "70", "71", "72", "73", "74", "76", "77", "78", "79", 
-"80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", 
-"91", "92", "93", "94", "95", "96", "98", "99", "100", "101", 
-"102", "103", "104", "105", "106", "107", "108", "110", "111", 
-"112", "113", "114", "115", "116", "117", "118", "119", "120", 
-"121", "122", "123", "124", "126"), c("A", "B", "C", "D", "E", 
-"F", "G")))
-
-
-
+plot.oconnell <- function(obj, type=c("p1"), xlab=NULL, ylab=NULL, main=NULL, ...) {
+    type <- match.arg(type)
+    if (type=="p1") {
+        if (is.null(xlab)) xlab <- "Rater"
+        if (is.null(ylab)) ylab <- "Probability"
+        if (is.null(main)) main <- ""
+        graphics:::plot.table(obj$p1, xlab=xlab, ylab=ylab, main=main, ...)
+    }
+}
